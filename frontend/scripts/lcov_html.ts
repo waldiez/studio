afedc01dff94dbf2012baa28590c0404ea97e73e
Lazaros Toumanidis <laztoum@protonmail.com>
2024-12-23 13:30:53 +0200

    update frontend/scripts/lcov_html.ts

--- frontend/scripts/lcov_html.ts
+++ frontend/scripts/lcov_html.ts
@@ -2,6 +2,7 @@
  * Generate HTML coverage report from lcov.info
  * Only if lcov.info exists
  */
+import packageJson from "../../package.json";
 import { execSync } from "child_process";
 import fs from "fs-extra";
 import path from "path";
@@ -11,13 +12,18 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const __rootDir = path.resolve(__dirname, "..", "..");
 
+function getPackageManager() {
+    const ifNotFound = "yarn";
+    return packageJson.packageManager || ifNotFound;
+}
+
 const main = () => {
     const lcovPath = path.resolve(__dirname, "..", "..", "coverage", "lcov.info");
     if (!fs.existsSync(lcovPath)) {
         console.info("No lcov.info found. Skipping HTML report generation");
         process.exit(0);
     }
-    execSync("yarn lcov:html", { stdio: "inherit", cwd: __rootDir });
+    execSync(`${getPackageManager()} run lcov:html`, { stdio: "inherit", cwd: __rootDir });
 };
 
 main();
